0ea6d1cb771ef01327a6cb8bdef4c31614214b39
Mike Barmettler <mike.barmettler@chainiq.com>
2016-10-20 16:29:48 +0200

    code cleanup and added margn to grid - because of footer concatination

--- WhoLends/WhoLends.Web/ViewModels/ManageViewModels.cs
+++ WhoLends/WhoLends.Web/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace WhoLends.Models
+namespace WhoLends.ViewModels
 {
     public class IndexViewModel
     {
